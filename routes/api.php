62abc9eef06baf7a3b6a557ce60169750b39f034
SuhaybHamdy <40205607+SuhaybHamdy@users.noreply.github.com>
2021-12-27 23:42:53 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,15 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
+
 Route::prefix('auth')->group(function () {
     //api/auth/register
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+    
     Route::group(['middleware' => ['auth:sanctum']], function () {
-        // Route::get('/user', AuthController::class, 'user');
-        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/user', [AuthController::class, 'user']);
+        Route::get('/logout', [AuthController::class, 'logout']);
     });
     Route::get('/authentication-failed', [AuthController::class, 'authFailer'])->name('auth-failed');
 
